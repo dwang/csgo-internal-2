236bf8c96bee8fff7a0ebbcf3bc90884eee20ff9
Daniel Wang <hello@danielwang.dev>
2019-01-20 13:01:46 -0500

    feat: create dll entry point and allocate console

---
+++ csgo-internal/core/main.cpp
@@ -0,0 +1,31 @@
+#include "../dependencies/common_includes.hpp"
+
+unsigned long __stdcall on_dll_attach(void* reserved)
+{
+	AllocConsole();
+	freopen_s((FILE**)stdin, "CONIN$", "r", stdin);
+	freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
+
+	while (!GetAsyncKeyState(VK_END))
+		std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+	FreeLibraryAndExitThread(reinterpret_cast<HMODULE>(reserved), 0);
+	return 0ul;
+}
+
+void __stdcall on_dll_detach()
+{
+	fclose((FILE*)stdin);
+	fclose((FILE*)stdout);
+	FreeConsole();
+}
+
+bool __stdcall DllMain(void* instance, unsigned long reason_to_call, void* reserved)
+{
+	if (reason_to_call == DLL_PROCESS_ATTACH)
+		CreateThread(0, 0, on_dll_attach, instance, 0, 0);
+	else if (reason_to_call == DLL_PROCESS_DETACH)
+		on_dll_detach();
+
+	return true;
+}
---
+++ csgo-internal/dependencies/common_includes.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#define VC_EXTRALEAN
+#include <Windows.h>
+#include <iostream>
+#include <thread>
